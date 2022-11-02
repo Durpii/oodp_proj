61c7dbf546b2ae9a08ae83e064e86ca79f298aac
Lim Zi Hao <limzihao1999@hotmail.com>
2022-11-02 13:49:11 +0800

    Added User, Staff files

---
+++ oodp_project/src/users/Staff.java
@@ -0,0 +1,19 @@
+package users;
+
+public class Staff extends User{
+	private int staffNo;
+	private String password;
+	
+	public Staff() {
+		
+	}
+	
+	public Staff(int age, String name, int staffNo) {
+		this.age = age;
+		this.name = name;
+		this.staffNo = staffNo;
+	}
+	
+	
+
+}
---
+++ oodp_project/src/users/User.java
@@ -0,0 +1,15 @@
+package users;
+
+public class User {
+	protected int age;
+	protected String name;
+	
+	public User() {
+		
+	}
+	
+	public User(int age, String name) {
+		this.age = age;
+		this.name = name;
+	}
+}

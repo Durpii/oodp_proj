decee5c64151c1d35bad65e1a19b720444e2691f
Jin Kee <miyagi5555@hotmail.com>
2022-11-12 04:09:11 +0800

    Updated ReviewController
    Created a function addReview

--- oodp_project/src/controllers/ReviewController.java
+++ oodp_project/src/controllers/ReviewController.java
@@ -1,31 +1,76 @@
 package controllers;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 import cinema.Movie;
 import cinema.Review;
+import users.User;
 
 public class ReviewController {
 	
 	private Movie movie;
 	private String reviewerName, review;
-	private int reviewRating;
+	private int reviewRating, uid;
+	public final String SEPARATOR = "@@@";
 
-	public ReviewController(Movie movie){
+	public ReviewController(Movie movie, User user){
 		this.movie = movie;
+		this.uid = user.getUserId();
 	}
 	
 	public void addReview() {
-		Scanner sc = new Scanner(System.in);
-		
-		System.out.print("Please enter your name: ");
-		reviewerName = sc.next();
-		System.out.print("Please enter your review: ");
-		review = sc.next();
-		System.out.print("Please enter your rating (1 ~ 5): ");
-		reviewRating = sc.nextInt();
+		try (Scanner sc = new Scanner(System.in)) {
+			System.out.print("Please enter your name: ");
+			reviewerName = sc.next();
+			System.out.print("Please enter your review: ");
+			review = sc.next();
+			System.out.print("Please enter your rating (1 ~ 5): ");
+			reviewRating = sc.nextInt();
+		}
 		
 		Review mreview = new Review(movie, reviewerName, review, reviewRating);
+		
+		FileWriter fileWriter = null;
+		
+		try {
+			File reviewFile = new File("reviews.txt");
+			fileWriter = new FileWriter("reviews.txt", true);
+			
+			if (reviewFile.createNewFile()) {
+				System.out.println("File created: " + reviewFile.getName());
+			}
+			
+			System.out.println("Adding your review...");
+			fileWriter.write("$ID:" + uid + "@@@" + movie.getId() + "@@@" + mreview.getReviewRating() 
+			+ "@@@" + mreview.getReview() + "\n");
+			
+		}
+		catch(FileNotFoundException e){
+			System.out.println("File not found!");
+			e.printStackTrace();
+		}
+		catch (IOException e) {
+			System.out.println("Error adding review, please try again");
+			e.printStackTrace();
+		}
+		finally {
+			if (fileWriter != null) {
+				try {
+					fileWriter.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+				
 	}
 
 }

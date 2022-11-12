3a53157d8091d2d5cf43f00d3c825c7ef6819ab7
Jin Kee <miyagi5555@hotmail.com>
2022-11-12 23:53:12 +0800

    Edited Cineplex constructor
    Added function to get cinema by id

--- oodp_project/src/cinema/Cineplex.java
+++ oodp_project/src/cinema/Cineplex.java
@@ -1,12 +1,32 @@
 package cinema;
 
+import java.util.ArrayList;
+
 public class Cineplex {
 
 	private String cineplexName;
 	private int cineplexID;
 	
+	ArrayList<Cinema> cinemas = new ArrayList<Cinema>();
+	
 	public Cineplex() {
 		//empty constructor
+	}
+	
+	//construct a Cineplex object with 4 Cinemas (3 Regular, 1 Platinum)
+	public Cineplex(String cineplexName, int cineplexID) {
+		this.cineplexName = cineplexName;
+		this.cineplexID = cineplexID;
+		
+		//initialise with 3 regular cinemas and 1 platinum cinema
+		for(int i=0; i<3; i++) {
+			Cinema cinema = new Cinema(this, i, CinemaClass.REGULAR);
+			cinemas.add(cinema);
+		}
+		
+		Cinema cinema = new Cinema(this, 3, CinemaClass.PLATINUM);
+		cinemas.add(cinema);
+		
 	}
 	
 	public String getCineplexName() {
@@ -22,4 +42,7 @@ public class Cineplex {
 		this.cineplexID = cineplexID;
 	}
 	
+	public Cinema getCinemaById(int id) {
+		return cinemas.get(id);
+	}
 }
